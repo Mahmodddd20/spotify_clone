a71f87d03cf587ea6fd68075e079ed1773b3a40b
mahmodddd20 <mahmodddd20@hotmail.com>
2021-07-19 07:07:51 +0300

    install react router and create basic route structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import './index.css';
+import Login from "./pages/Login";
+import Search from "./pages/Search";
+import Albums from "./pages/Albums";
 
-function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
+
+export default function App() {
+    return (
+        <div className="container mx-auto h-screen px-4 m-4">
+            <Router>
+                <Route exact path="/" component={Login}/>
+                <Route exact path="/albums" component={Albums}/>
+                <Route exact path="/search" component={Search}/>
+            </Router>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/pages/Albums.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Albums(props) {
+    return (
+        <div>Albums</div>
+    );
+}
+
+export default Albums;
