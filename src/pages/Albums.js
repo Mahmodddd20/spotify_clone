805e86eb4a02f9141dc48a129720f6e1239fffaf
mahmodddd20 <mahmodddd20@hotmail.com>
2021-07-19 07:17:01 +0300

    create the albums design

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Albums(props) {
     return (
-        <div>Albums</div>
+        <div className="grid grid-row-7 container ">
+            <div
+                className="m-2 w-72 px-2 py-2 text-gray-900">
+                <h1 className="w-full text-2xl">Name</h1>
+                <p className="w-full text-sm text-gray-500">Albums</p>
+            </div>
+        </div>
     );
 }
 
