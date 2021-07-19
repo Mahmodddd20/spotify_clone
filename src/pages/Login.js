018a6a214b0af1b3fc925ebb52e0e4f3e177cba7
mahmodddd20 <mahmodddd20@hotmail.com>
2021-07-19 07:25:23 +0300

    edit login page design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,10 +20,10 @@ function Login() {
     };
 
     return (
-        <div className="grid grid-col-1 grid-row-1 h-full place-items-center 	">
+        <div className="grid grid-col-1 grid-row-1 ">
             <button onClick={handleLogin}
                     className=" border border-gray-400 w-60 px-2 py-2 rounded m-4 shadow-sm text-2xl text-gray-900 ">
-                <span className="flex w-full px-4 justify-center relative">
+                <span className="flex w-full px-4  relative">
                     Login
                     <img alt="icon" className="h-6 w-6 absolute right-0" src={icon}/>
                 </span>
