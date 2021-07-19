ca8fb7744d676043d35586ec9cd7aaf49e738804
mahmodddd20 <mahmodddd20@hotmail.com>
2021-07-19 09:07:51 +0300

    add the id to album URL

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Login from "./pages/Login";
 import Search from "./pages/Search";
 import Albums from "./pages/Albums";
 
+
 export default function App() {
     return (
         <div className="container mx-auto h-screen px-4 m-4">
             <Router>
                 <Route exact path="/" component={Login}/>
-                <Route exact path="/albums" component={Albums}/>
+                <Route exact path="/albums/:id" component={Albums}/>
                 <Route exact path="/search" component={Search}/>
             </Router>
         </div>
