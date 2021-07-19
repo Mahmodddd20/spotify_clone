c9a1a3105054c407a23af3e8e4193139c4d81ff9
mahmodddd20 <mahmodddd20@hotmail.com>
2021-07-19 07:14:00 +0300

    create the login design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import icon from "../assets/Spotify_Icon.png"
 
-function Login(props) {
+function Login() {
     return (
-        <div>Login</div>
+        <div className="grid grid-col-1 grid-row-1 h-full place-items-center 	">
+            <button className=" border border-gray-400 w-60 px-2 py-2 rounded m-4 shadow-sm text-2xl text-gray-900 ">
+                <span className="flex w-full px-4 justify-center relative">
+                    Login
+                    <img alt="icon" className="h-6 w-6 absolute right-0" src={icon}/>
+                </span>
+            </button>
+        </div>
     );
 }
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function Search(props) {
     return (
         <div>Search</div>
