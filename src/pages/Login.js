9cd1bf4d1d6c2e1ae69ae7ada51a8c9428961f26
mahmodddd20 <mahmodddd20@hotmail.com>
2021-07-19 07:26:37 +0300

    make the login page responsive

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import icon from "../assets/Spotify_Icon.png"
 
-function Login() {
+export default function Login() {
     const CLIENT_ID = "93ff98fdec2345148e7b120be10be630"
     const SPOTIFY_URL = "https://accounts.spotify.com/authorize"
     const PROJECT_URL = "http://localhost:3000/"
@@ -14,16 +14,15 @@ function Login() {
 
     const SCOPES_WITH_SPACES = SCOPES.join(SPACE_BETWEEN_SCOPES);
 
-
     function handleLogin() {
         window.location = `${SPOTIFY_URL}?client_id=${CLIENT_ID}&redirect_uri=${PROJECT_URL}search&scope=${SCOPES_WITH_SPACES}&response_type=token&show_dialog=true`;
     };
 
     return (
-        <div className="grid grid-col-1 grid-row-1 ">
+        <div className="grid grid-col-1 grid-row-1 h-full place-items-center 	">
             <button onClick={handleLogin}
                     className=" border border-gray-400 w-60 px-2 py-2 rounded m-4 shadow-sm text-2xl text-gray-900 ">
-                <span className="flex w-full px-4  relative">
+                <span className="flex w-full px-4 justify-center relative">
                     Login
                     <img alt="icon" className="h-6 w-6 absolute right-0" src={icon}/>
                 </span>
@@ -32,4 +31,3 @@ function Login() {
     );
 }
 
-export default Login;
