ac524cf003a76a6d5ea567a6c76927aa47ec4529
mahmodddd20 <mahmodddd20@hotmail.com>
2021-08-25 19:28:45 +0300

    delete githup pages support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 import './index.css';
 import Login from "./pages/Login";
 import Search from "./pages/Search";
@@ -8,11 +8,11 @@ import Albums from "./pages/Albums";
 export default function App() {
     return (
         <div className="container mx-auto h-screen px-4 m-4">
-            <Router>
+            <BrowserRouter>
                 <Route exact path="/" component={Login}/>
                 <Route exact path="/albums/:id" component={Albums}/>
                 <Route exact path="/search" component={Search}/>
-            </Router>
+            </BrowserRouter>
         </div>
     );
 }
